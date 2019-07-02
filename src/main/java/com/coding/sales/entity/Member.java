31cafac68906933d3bab919520307c86495f6cc4
wenbing <wb.wenbing@gmail.com>
2019-07-02 17:26:09 +0800

    创建实体类

---
+++ src/main/java/com/coding/sales/entity/Member.java
@@ -0,0 +1,54 @@
+package com.coding.sales.entity;
+
+import java.math.BigDecimal;
+
+public class Member {
+    /**
+     * 会员卡号
+     */
+    private String memberId;
+    /**
+     * 会员姓名
+     */
+    private String memberName;
+    /**
+     * 会员等级
+     */
+    private String memberType;
+    /**
+     * 会员积分
+     */
+    private BigDecimal memberPoints;
+
+    public String getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(String memberId) {
+        this.memberId = memberId;
+    }
+
+    public String getMemberName() {
+        return memberName;
+    }
+
+    public void setMemberName(String memberName) {
+        this.memberName = memberName;
+    }
+
+    public String getMemberType() {
+        return memberType;
+    }
+
+    public void setMemberType(String memberType) {
+        this.memberType = memberType;
+    }
+
+    public BigDecimal getMemberPoints() {
+        return memberPoints;
+    }
+
+    public void setMemberPoints(BigDecimal memberPoints) {
+        this.memberPoints = memberPoints;
+    }
+}
